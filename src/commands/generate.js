58f6c6bc76de3320b19d3a81d4179df2888ed9f3
BoyWithSilverWings <agney@outlook.in>
2018-12-23 14:57:59 +0530

    generate from ejs files

--- src/commands/generate.js
+++ src/commands/generate.js
@@ -8,20 +8,45 @@ module.exports = {
       template: { generate },
       print: { info },
       validateName,
+      filesystem,
+      system,
     } = toolbox;
 
     const name = parameters.first;
+    const props = { name };
     
     if(!validateName(name)) {
       return;
     }
 
-    await generate({
-      template: 'model.js.ejs',
-      target: `models/${name}-model.js`,
-      props: { name }
-    })
+    await filesystem.dir(props.name)
 
-    info(`Generated file at models/${name}-model.js`)
+    const files = [
+      'bin/card.js.ejs',
+      'package.json.ejs',
+      'README.md.ejs',
+      '.gitignore.ejs',
+    ];
+
+    const filesCopy = files.reduce((acc, file) => {
+      const template = `/${file}`;
+      const target = `${props.name}/${file.replace('.ejs', '')}`;
+      const gen = generate({ template, target, props });
+      return acc.concat([gen]);
+    }, []);
+
+    await Promise.all(filesCopy);
+
+    filesystem.chmodSync(`${props.name}/bin/card.js`, '755');
+
+    const npmPath = system.which('npm');
+
+    await system.spawn(`cd ${props.name} && ${npmPath} install --silent && ${npmPath} run --quiet format`, {
+      shell: true,
+      stdio: 'inherit',
+      stderr: 'inherit',
+    });
+
+    info(`Generated profile card`)
   }
 }
