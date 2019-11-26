93fffb86750e25ee513fc4734a8331d4c6a99a31
Boy with Silver Wings <agney@outlook.in>
2019-11-26 23:31:38 +0530

    update tests

--- __tests__/cli-integration.test.js
+++ __tests__/cli-integration.test.js
@@ -7,24 +7,16 @@ const cli = async cmd =>
   system.run('node ' + resolve(src, 'bin', 'create-profile-card') + ` ${cmd}`)
 
 test('outputs version', async () => {
-  const output = await cli('--version')
-  expect(output).toContain('0.0.1')
+
+  // Original output as terminal control characters that do not match, hence starting and ending characters are removed from original regex. 
+  // https://semver.org/#is-there-a-suggested-regular-expression-regex-to-check-a-semver-string
+  const semVerRegex = /(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?/gm;
+  const output = await cli('--version');
+
+  expect(semVerRegex.test(output)).toBe(true);
 })
 
 test('outputs help', async () => {
   const output = await cli('--help')
-  expect(output).toContain('0.0.1')
-})
-
-test('generates file', async () => {
-  const output = await cli('generate foo')
-
-  expect(output).toContain('Generated file at models/foo-model.js')
-  const foomodel = filesystem.read('models/foo-model.js')
-
-  expect(foomodel).toContain(`module.exports = {`)
-  expect(foomodel).toContain(`name: 'foo'`)
-
-  // cleanup artifact
-  filesystem.remove('models')
+  expect(output).toContain('help')
 })
